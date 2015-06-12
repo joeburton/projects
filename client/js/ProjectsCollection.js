06c9884dd7b4dbcd6cc86f7fa6c0aae2a83e1ef2
joe burton <joeburton@gmail.com>
2015-06-12 11:20:37 +0100

    backbone, require

--- client/js/ProjectsCollection.js
+++ client/js/ProjectsCollection.js
@@ -8,8 +8,22 @@ define([
     
 	var ProjectsCollection = Backbone.Collection.extend({
 
-		url: 'http://localhost:3000/projects',
+		url: function () {
+			var production = true;
+			var url;
 
+			if (production) {
+			    // production
+			    url = 'http://projects.joe-burton.com/projects';
+			} else {
+			    // local dev
+			    url = 'http://localhost:3000/projects';
+			}
+			
+			return url;
+
+		},
+		
 		model: ProjectModel
 
 	});
