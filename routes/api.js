408d9d7d9cf16c0fb8a9ec04671d34168256d43b
joe burton <joeburton@gmail.com>
2016-09-12 21:19:25 +0100

    update test

--- routes/api.js
+++ routes/api.js
@@ -38,7 +38,7 @@ exports.login = function (req, res) {
     sess.user = req.body.user;
     sess.password = req.body.password;
 
-    if (sess.user === 'admin' && sess.password === 'secret') {
+    if (sess.user === 'admin' && sess.password === 'projects') {
         console.log('Login Successful');
         sess.authenticated = true;
         res.redirect('/admin');
